0dd7c25e9866127ac53886e12733b032e0ada86e
OlgaVysochanska <olavysochanska@gmail.com>
2022-11-10 21:18:49 +0200

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,22 +5,51 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
-const newImg = ({ preview, description }) => `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+const newImg = ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="large-image.jpg"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
 </div>`
-;
+  ;
+
+  
  
 const makeGallery = galleryItems.map(newImg).join("");
 
 gallery.insertAdjacentHTML("afterbegin", makeGallery);
 
-const instance = basicLightbox.create(``);
 
-instance.show();
+const instance = basicLightbox.create(`<img class="modal-image" src="" width="800" height="600">`);
+
+const modalImage = instance.element().querySelector(".modal-image");
+
+function handleGalleryClick(e) {
+  if (e.target === e.currentTarget) 
+    return;
+  e.preventDefault();
+
+  const originalImgSrc = e.target.dataset.source;
+
+  viewOriginalImage(originalImgSrc);
+}
+
+function viewOriginalImage(src) {
+
+  modalImage.src = src;
+  instance.show();
+}
+
+function onEscapePress(e) {
+  if (e.code === "Escape") {
+    instance.close();
+  }
+}
+
+gallery.addEventListener("click", handleGalleryClick);
+modalImage.addEventListener("click", instance.close);
+window.addEventListener("keydown", onEscapePress);
